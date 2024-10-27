311c701ab9aadb1ddd52e29b57f57a2d0420f309
peekybastards <peekybastards@gmail.com>
2024-10-28 05:01:06 +0530

    init ringjson to parse in json post bodies and json response

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"] [clj-http "3.13.0"] [cheshire "5.13.0"] [ring/ring-jetty-adapter "1.13.0"] [ring/ring-core "1.13.0"] [metosin/reitit-ring "0.2.3"] [ring/ring-json "0.5.1"]]
   :main ^:skip-aot rateit.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/rateit/core.clj
+++ src/rateit/core.clj
@@ -1,7 +1,30 @@
 (ns rateit.core
+  (:require [ring.adapter.jetty :as jetty] [reitit.ring :as reitit] [cheshire.core :as json] [ring.middleware.json :refer [wrap-json-response wrap-json-body]] [ring.util.response :refer [response]])
   (:gen-class))
+
+(defn ping-handler [_] 
+  (response {:success true 
+              :error false 
+              :message "sent you a ping masta"}))
+
+
+
+(defn post-handler [request] 
+  (let [data (:body request)]
+    (response (json/generate-string data))))
+
+
+
+
+
+(def routes (reitit/router ["/api" 
+                              ["/ping" {:handler ping-handler :middleware [wrap-json-response]}]
+                              ["/posty" {:handler post-handler :middleware [[wrap-json-body] [wrap-json-response]]}]
+                            ]))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (jetty/run-jetty (reitit/ring-handler routes) {:join? false :port 8091})
   (println "Hello, World!"))
